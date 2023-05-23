48652239507a29f8c6dc508cf7186cb5525b0d60
Leo Romero <leoromero.bls@gmail.com>
2023-05-23 14:16:07 -0300

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://leoromero1.github.io/ToDoList/'
+  base:'/ToDoList/'
 })
